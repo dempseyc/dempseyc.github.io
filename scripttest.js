b3156e40a98cf9b9a84788d7823823c4272eb732
Craig Dempsey <craigdempsey@gmail.com>
2017-03-14 13:32:33 -0400

    work ui play action on avatar, complete craig step 1

--- scripttest.js
+++ scripttest.js
@@ -14,7 +14,16 @@ let A = {
   handle: hAVT
 };
 
-let amount = 16;
+let amount = 40;
+
+let hAVTchangePos = function (x,y) {
+  hAVT.css({
+    "top": A.yTarget, //dont let it reach 0
+    "left": A.xTarget //dont let it reach 0
+  });
+}
+
+hAVTchangePos(A.xTarget,A.yTarget);
 
 let moveAVT = function(d) {
 
@@ -23,18 +32,22 @@ let moveAVT = function(d) {
     case "up":
       A.yTarget -= amount;
       A.yPos = A.yTarget;
+      break;
 
     case "dn":
       A.yTarget += amount;
       A.yPos = A.yTarget;
+      break;
 
     case "left":
       A.xTarget -= amount;
       A.xPos = A.xTarget;
+      break;
 
     case "right":
       A.xTarget += amount;
       A.xPos = A.xTarget;
+      break;
 
   }//end switch
 
@@ -44,3 +57,30 @@ let moveAVT = function(d) {
   });
 
 }
+
+// copied template from
+// http://stackoverflow.com/questions/1402698/binding-arrow-keys-in-js-jquery
+
+$(document).keydown(function(e) {
+  //alert(String.fromCharCode(e.keyCode));
+    switch(e.which) {
+        case 37: // left
+        moveAVT("left");
+        break;
+
+        case 38: // up
+        moveAVT("up");
+        break;
+
+        case 39: // right
+        moveAVT("right");
+        break;
+
+        case 40: // down
+        moveAVT("dn");
+        break;
+
+        default: return; // exit this handler for other keys
+    }//end switch statement
+    e.preventDefault(); // prevent the default action (scroll / move caret)
+});
